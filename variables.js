142ce9c50fb69ec3a886ffa93792171f00dc95fb
Brandon Ainsworth <brandonaainsworth@gmail.com>
2022-02-07 20:14:37 -0700

    Add numbers

--- variables.js
+++ variables.js
@@ -1,3 +1,6 @@
 var spaceSize = "Way too big."
 var coolestPlanet = "Earth. If you say otherwise you do not even exist."
 var spaceSmell = "Burnt Steak"
+var suns = 1
+var earthMoons = 1
+var spaceJunk = 30000
